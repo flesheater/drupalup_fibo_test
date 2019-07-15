87c12b1186d3acb7807ebc3a5428ecf9ae46fb4e
flesheater <nikolay.r.borisov@gmail.com>
2019-07-16 01:04:06 +0200

    The tests for the fibonacci service

---
+++ tests/src/Unit/FibonacciServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\Tests\drupalup_fibo\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\drupalup_fibo\FibonacciService;
+
+/**
+ * Our main test class for the fibonacci service.
+ */
+class FibonacciServiceTest extends UnitTestCase {
+
+  public function testSixthFibonacciNumber() {
+    $fibonacciService = new FibonacciService();
+    $fibonacciSequence = $fibonacciService->calcSomeFibos(6);
+    $this->assertEquals(
+      $fibonacciSequence[5],
+      5
+    );
+  }
+
+}
